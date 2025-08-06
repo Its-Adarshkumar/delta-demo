90ac864530f59b196b6d9a87819322d055163250
Its-Adarshkumar <adarshsinghrajput414@gmail.com>
2025-08-07 00:40:56 +0530

    first commit

---
+++ sum.test.js
@@ -0,0 +1,13 @@
+const sum=require("./sum.js")
+
+
+describe("test for sum fnx",()=>{
+    test('adds 2 +2 to equal 4',()=>{
+        expect(sum(2,2)).toBe(4);
+    });
+
+    test('adds -5 + -5 to equal -10',()=>{
+        expect(sum(-5,-5)).toBe(-10);
+    });
+})
+
